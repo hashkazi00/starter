715bf035e1caf660431f4745b4959baa22d9f846
hashkazi00 <sahilkazi01@hotmail.com>
2020-11-14 23:10:50 +0530

    Handled Rejection Error and Uncaught Exception Error Handled

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+
+process.on('uncaughtException', err => {
+    console.log('Uncaught Exception Error')
+
+    console.log(err.name, err.message);
+    process.exit(1);
+})
+
 dotenv.config({ path: './config.env' })
 
 //IF CURIOS ABOUT WHY WE DID NOT READ THE FILE TO REPLACE, NOTE THAT WE READ THE DOTENV IN THE ABOVE LINE AND WE AHVE ITS CONTENTS OVER HERE
@@ -23,6 +31,18 @@ mongoose.connect(DB, { // this object is just top avoid a few deprecation warnin
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 })
+
+
+process.on('unhandledRejection', err => {
+    console.log('Unhandled Rejection Error')
+
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1)
+    })
+})
+
+
