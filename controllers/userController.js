a472f2ac87ae90a9677934d3e21d09470c010424
hashkazi00 <sahilkazi01@hotmail.com>
2020-12-24 16:34:36 +0530

    Implemented Logged in  User Data Update Route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,18 @@
+const AppError = require('../utils/appError');
+const { findByIdAndUpdate } = require('./../models/userModel');
 const User = require('./../models/userModel')
 
 const catchAsync = require('./../utils/catchAsync')
+
+const filterObj = (obj, ...allowedFields) => {
+    let newObj = {};
+    console.log(obj);
+    Object.keys(obj).forEach(el => {
+        if(allowedFields.includes(el)) newObj[el] = obj[el];
+    })
+    console.log(newObj)
+    return newObj;
+}
 
 exports.getAllUsers = catchAsync( async(req, res, next) => {
     const users = await User.find();
@@ -10,6 +22,29 @@ exports.getAllUsers = catchAsync( async(req, res, next) => {
         status: 'success',
         results: users.length, //this property is a good practice wherever obvious and has nothing to do with JSend
         data: { users } //tours implies the name of the resources in the url
+    })
+});
+
+exports.updateMe = catchAsync( async (req, res, next) => {
+    //Create error if user posts password data
+    if(req.body.password || req.body.confirmPassword){
+        return next(new AppError('You cannot update password from this route!!', 400))
+    } 
+
+    //Update the user document
+    //We'll use .findByIdAndUpdate() instead of .save(), as save leads to validation and that way we cannot partially update the document
+
+
+    const filteredBody = filterObj(req.body, 'name','email');
+
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        runValidators:true,
+        new:true
+    })
+
+    res.status(200).json({
+        status:'success',
+        updatedUser
     })
 });
 
